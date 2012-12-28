c0b9f44d9d41da2e3edfccf915a66273e2270ce4
moradaniel <moradaniel@gmail.com>
2012-12-28 10:13:54 -0300

    Initial commit

---
+++ app/models/purchase.rb
@@ -0,0 +1,6 @@
+class Purchase < ActiveRecord::Base
+  attr_accessible :cost, :name
+
+  validates_presence_of :name
+  validates_numericality_of :cost, :greater_than => 0
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanjuanar::Application.config.secret_token = 'e31864b6b51ba3290c421ee50aa482ca33f27fd1ed39aaac6499d92d8fa0a639c3a5a6401cc26d11347bbc2be7e375bdb3e994d114ae787b7ebcfac6e44f85d0'
