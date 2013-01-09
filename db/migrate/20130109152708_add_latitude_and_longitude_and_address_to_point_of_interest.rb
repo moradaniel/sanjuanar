ce87eaeb98b42b8d9d3d8f5ad1e26674945a5d28
moradaniel <moradaniel@gmail.com>
2013-01-09 13:20:38 -0300

    Added default values to latitude and longitude

--- db/migrate/20130109152708_add_latitude_and_longitude_and_address_to_point_of_interest.rb
+++ db/migrate/20130109152708_add_latitude_and_longitude_and_address_to_point_of_interest.rb
@@ -1,7 +1,7 @@
 class AddLatitudeAndLongitudeAndAddressToPointOfInterest < ActiveRecord::Migration
   def change
-    add_column :point_of_interests, :lat, :float, :null => false
-    add_column :point_of_interests, :lng, :float, :null => false
+    add_column :point_of_interests, :lat, :float, :null => false  , :default => -31.5380
+    add_column :point_of_interests, :lng, :float, :null => false  , :default => -68.5237
     add_column :point_of_interests, :address, :string
   end
 end
